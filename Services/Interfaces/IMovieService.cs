9972762f8556a1efb67da4e08685e7610eda30a3
ShanksCode <jnc201820@gmail.com>
2023-05-23 20:39:17 -0300

    feat: Adicionado metodo services

--- Services/Interfaces/IMovieService.cs
+++ Services/Interfaces/IMovieService.cs
@@ -1,6 +1,13 @@
-﻿namespace Api_movie.Services.Interfaces
+﻿using Api_movie.Models;
+
+namespace Api_movie.Services.Interfaces
 {
     public interface IMovieService
     {
+        Task<List<Movie>> GetAllMoviesAsync();
+        Task<Movie> GetMovieByIdAsync(int id);
+        Task<Movie> AddMovieAsync(Movie movie);
+        Task DeleteMovieAsync(Movie movie);
+        Task<Movie> UpdateMovieAsync(Movie movie);
     }
 }
--- Services/MovieService.cs
+++ Services/MovieService.cs
@@ -16,33 +16,34 @@ namespace Api_movie.Services
         public async Task<Movie> AddMovieAsync(Movie movie)
         {
 
-            var movieAdded = await _repository.GetAllMoviesAsync(movie.Name);
+            var movieAdded = await _repository.GetMovieByNameAsync(movie.Name);
                 
             if (movieAdded != null)
             {
-                throw new Exception("O nome do filme já existe!");
+                throw new Exception("Esse filme já foi cadastrado.");
             }
-            return movie;
+            return await _repository.AddMovieAsync(movie);
 
         }
 
         public async Task DeleteMovieAsync(Movie movie)
         {
-
+            await _repository.DeleteMovieAsync(movie);
         }
 
         public async Task<List<Movie>> GetAllMoviesAsync()
         {
+            return await _repository.GetAllMoviesAsync();
         }
 
         public async Task<Movie> GetMovieByIdAsync(int id)
         {
-
+            return await _repository.GetMovieByIdAsync(id);
         }
 
         public async Task<Movie> UpdateMovieAsync(Movie movie)
         {
-
+            return await _repository.UpdateMovieAsync(movie);
         }
     }
 }
